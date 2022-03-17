02e2657d101b5eca11d30ec11e74d9cf7962cd3c
Yuta123456 <yuuta09090530@icloud.com>
2022-03-18 00:47:34 +0900

    pylintに怒られないよう修正

--- util/calc_L_z.py
+++ util/calc_L_z.py
@@ -1,18 +1,18 @@
 import sys
 import numpy as np
-def calc_L_z(n, Z, dist,  W):
+def calc_l_z(node_cnt, z_array, dist, weight):
     """
     L^zを求める。X^tからX^{t+1}を導出するのに必要な行列である。
 
     Parameters
     ----------
-    n : int
+    node_cnt : int
         ノード数
-    Z : np.ndarray
+    z_array : np.ndarray
         1step前の座標ベクトル
     dist : np.ndarray
         グラフの距離行列
-    W : np.ndarray
+    weight : np.ndarray
         重み係数行列である。
     alpha : int
         重みのパラメタ
@@ -21,20 +21,20 @@ def calc_L_z(n, Z, dist,  W):
     L^z : np.ndarray
         対象グラフの重み係数行列
     """
-    
-    L_z = np.empty((n, n))
-    for i in range(n):
-        for j in range(n):
+
+    l_z = np.empty((node_cnt, node_cnt))
+    for i in range(node_cnt):
+        for j in range(node_cnt):
             if i != j:
-                sub = (Z[i] - Z[j]) ** 2
+                sub = (z_array[i] - z_array[j]) ** 2
                 sub_sum = sum(sub)
                 if sub_sum == 0:
                     sub_inv = 0
                 else:
                     sub_inv = 1 / sum(sub)
-                L_z[i][j] = -W[i][j] * dist[i][j] * sub_inv
-    
-    for i in range(n):
-        L_z[i][i] = - sum([L_z[i][j] if j != i else 0 for j in range(n)])
-    
-    return L_z
+                l_z[i][j] = -weight[i][j] * dist[i][j] * sub_inv
+
+    for i in range(node_cnt):
+        l_z[i][i] = - sum([l_z[i][j] if j != i else 0 for j in range(node_cnt)])
+
+    return l_z
