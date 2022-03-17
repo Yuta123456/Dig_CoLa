0bde16a6a1b76a2b6397e24af47574be1ee6912f
Yuta123456 <yuuta09090530@icloud.com>
2022-03-18 00:52:05 +0900

    test関数を修正

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
 import unittest
-from util.calc_weight import calc_weight 
 import numpy as np
-
+from util.calc_weight import calc_weight
+from util.calc_dist import calc_dist
+from util.calc_L_w import calc_l_w
 class Test(unittest.TestCase):
-    def test_calc_weight(self):
+    """
+    テスト用クラス
+    """
+    def test_calc_l_w(self):
+        """
+        calc_L_w関数のテスト用関数
+        """
+        node_cnt = 3
         edges = [[0,1], [1,2]]
-        res = calc_weight(3, edges)
+        dist = calc_dist(node_cnt, edges)
+        weight = calc_weight(dist)
+        res = calc_l_w(node_cnt, weight)
         ans = np.array([[5/4, -1, -1/4], [0, 1, -1], [0, 0, 0]])
         # self.assertEqual(res, ans)
         self.assertEqual(res.shape, ans.shape)
