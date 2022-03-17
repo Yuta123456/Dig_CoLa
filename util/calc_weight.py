6d204e9052d0ae8f9b91836d435f63418cce5b53
Yuta123456 <yuuta09090530@icloud.com>
2022-03-18 00:48:31 +0900

    pylintに怒られないよう修正

--- util/calc_weight.py
+++ util/calc_weight.py
@@ -15,9 +15,9 @@ def calc_weight(dist, alpha=-2):
     W : np.ndarray
         対象グラフの重み係数行列
     """
-    W = np.empty(dist.shape)
+    weight = np.empty(dist.shape)
     for i in range(dist.shape[0]):
         for j in range(dist.shape[1]):
             # TODO: dist[i][j] = 0の時コケるかも
-            W[i][j] = pow(dist[i][j], alpha)
-    return W
+            weight[i][j] = pow(dist[i][j], alpha)
+    return weight
