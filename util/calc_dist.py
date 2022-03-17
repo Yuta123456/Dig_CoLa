aa602b3d5c06c9dd89352482f7dfd9eb8ad09558
Yuta123456 <yuuta09090530@icloud.com>
2022-03-18 00:44:13 +0900

    pylintに怒られないように修正

--- util/calc_dist.py
+++ util/calc_dist.py
@@ -1,8 +1,7 @@
-import sys
 import numpy as np
 from scipy.sparse.csgraph import shortest_path
 
-def calc_dist(n, edges):
+def calc_dist(node_cnt, edges):
     """
     グラフの最短経路を計算。
 
@@ -11,25 +10,22 @@ def calc_dist(n, edges):
     n : int
         ノード数
     edges : np.ndarray
-        辺の接続情報 
+        辺の接続情報
     Returns
     -------
     L_w : np.ndarray
         対象グラフの重み係数行列
     """
     # ノードの接続情報から隣接行列を作成
-    adjusent_list = [[0 for j in range(n)] for i in range(n)]
-    for e in edges:
-        src = e[0]
-        tar = e[1]
+    adjusent_list = [[0 for j in range(node_cnt)] for i in range(node_cnt)]
+    for edge in edges:
+        src = edge[0]
+        tar = edge[1]
         try: 
-            weight = e[2]
+            weight = edge[2]
         except IndexError:
             # 重みが設定されていない場合には、重みなし辺と認識し、1を設定
             weight = 1
-        except Exception as e:
-            print(e)
-            sys.exit(1)
 
         adjusent_list[src][tar] = weight
     dist = shortest_path(np.array(adjusent_list))
