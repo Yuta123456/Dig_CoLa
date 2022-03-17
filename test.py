1d45c85f140d9291111f07ffb4e3a32a27e62a15
Yuta123456 <yuuta09090530@icloud.com>
2022-03-18 01:44:24 +0900

    行列を見やすく定義

--- test.py
+++ test.py
@@ -16,11 +16,43 @@ class Test(unittest.TestCase):
         dist = calc_dist(node_cnt, edges)
         weight = calc_weight(dist)
         res = calc_l_w(node_cnt, weight)
-        ans = np.array([[5/4, -1, -1/4], [0, 1, -1], [0, 0, 0]])
+        ans = np.array([[5/4, -1, -1/4],
+                        [0, 1, -1],
+                        [0, 0, 0]])
         # self.assertEqual(res, ans)
+        self.check_equal_numpy_array(res, ans)
+
+    def test_calc_dist(self):
+        """
+        calc_distのテスト用関数
+        """
+        node_cnt = 4
+        edges = [[0, 1], [1, 2], [2,3], [3,1]]
+        dist = calc_dist(node_cnt, edges)
+        ans = np.array(([[0, 1, 2, 3],
+                         [np.inf, 0, 1, 2],
+                         [np.inf, 2, 0, 1],
+                         [np.inf, 1, 2, 0]]))
+        self.check_equal_numpy_array(dist, ans)
+
+    def test_calc_weight(self):
+        """
+        calc_weightのテスト用関数
+        """
+        dist = np.array(([[0, 1, 2, 3],
+                         [np.inf, 0, 1, 2],
+                         [np.inf, 2, 0, 1],
+                         [np.inf, 1, 2, 0]]))
+        weight = calc_weight(dist)
+        ans = 1 / dist ** 2
+        self.check_equal_numpy_array(weight, ans)
+
+    def check_equal_numpy_array(self, res, ans):
+        """
+        numpy行列の一致比較
+        """
         self.assertEqual(res.shape, ans.shape)
         for i in zip(res.flatten(), ans.flatten()):
             self.assertEqual(i[0], i[1])
-
 if __name__ == "__main__":
     unittest.main()
