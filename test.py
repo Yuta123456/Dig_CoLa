cd8b22c7dd03db6b12a3cc0344bbb738ebfa4057
Yuta123456 <yuuta09090530@icloud.com>
2022-03-19 18:09:34 +0900

    関数の修正に伴うテストの修正

--- test.py
+++ test.py
@@ -20,8 +20,8 @@ class Test(unittest.TestCase):
         weight = calc_weight(dist)
         res = calc_l_w(node_cnt, weight)
         ans = np.array([[5/4, -1, -1/4],
-                        [0, 1, -1],
-                        [0, 0, 0]])
+                        [-1, 2, -1],
+                        [-1/4, -1, 5/4]])
         # self.assertEqual(res, ans)
         self.check_equal_numpy_array(res, ans)
 
@@ -32,20 +32,20 @@ class Test(unittest.TestCase):
         node_cnt = 4
         edges = [[0, 1], [1, 2], [2,3], [3,1]]
         dist = calc_dist(node_cnt, edges)
-        ans = np.array(([[0, 1, 2, 3],
-                         [np.inf, 0, 1, 2],
-                         [np.inf, 2, 0, 1],
-                         [np.inf, 1, 2, 0]]))
+        ans = np.array(([[0, 1, 2, 2],
+                         [1, 0, 1, 1],
+                         [2, 1, 0, 1],
+                         [2, 1, 1, 0]]))
         self.check_equal_numpy_array(dist, ans)
 
     def test_calc_weight(self):
         """
         calc_weightのテスト用関数
         """
-        dist = np.array(([[0, 1, 2, 3],
-                         [np.inf, 0, 1, 2],
-                         [np.inf, 2, 0, 1],
-                         [np.inf, 1, 2, 0]]))
+        dist = np.array(([[0, 1, 2, 2],
+                         [1, 0, 1, 1],
+                         [2, 1, 0, 1],
+                         [2, 1, 1, 0]]))
         weight = calc_weight(dist)
         ans = 1 / dist ** 2
         self.check_equal_numpy_array(weight, ans)
@@ -60,7 +60,9 @@ class Test(unittest.TestCase):
         weight = calc_weight(dist)
         pre_x = np.array([[1,2], [2,2], [1, 4], [1,3]])
         l_pre_x = calc_l_z(node_cnt, pre_x, dist, weight)
-        print(l_pre_x)
+        # TODO: 計算結果があっているか判断。
+
+
     def test_calc_obj(self):
         """
         calc_objのテスト用関数
@@ -73,7 +75,7 @@ class Test(unittest.TestCase):
         pre_x = np.array([[1,2], [2,2], [1, 4], [1,3]])
         l_pre_x = calc_l_z(node_cnt, pre_x, dist, weight)
         res = calc_obj(node_cnt, l_w, l_pre_x, pre_x[:, 0], 0)
-        print(res)
+        # TODO: 計算結果があっているか判断。
 
     def check_equal_numpy_array(self, res, ans):
         """
