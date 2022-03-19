675a17da5ff6b175ff9b6223c0f9501f03fd712c
Yuta123456 <yuuta09090530@icloud.com>
2022-03-19 21:43:06 +0900

    最適化したい数式を計算する関数が間違っていたので修正

--- util/calc_obj.py
+++ util/calc_obj.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sympy
 
-def calc_obj(node_cnt, l_w, l_pre_x, pre_x_at_a, axis):
+def calc_obj(node_cnt, l_w, l_pre_x, pre_x_at_a):
     """
     特定の次元の目的関数を計算
 
@@ -15,18 +15,15 @@ def calc_obj(node_cnt, l_w, l_pre_x, pre_x_at_a, axis):
         X(t-1)から導出される行列
     pre_x_at_a : np.ndarray
         X(t-1), つまり、前のノードの位置。そのa次元のみ抽出したベクトル
-    axis : int
-        次元
     Returns
     -------
-        TODO: なんか書く
+        TODO: 最適化したい数式
     """
     x = np.array([])
-    for i in range(node_cnt):
-        x = np.append(x, sympy.Symbol(f"x_{axis}{i}"))
+    for i in range(1, node_cnt+1):
+        x = np.append(x, sympy.Symbol(f"x_{i}"))
     obj = np.dot(np.dot(x.T, l_w), x)
-    print(f"\n\nx.T: {x.T.shape}, l_pre_x: {l_pre_x.shape}, pre_x_at_a: {pre_x_at_a.shape}")
-    print(f"\n\n{l_pre_x}\n\n")
     obj_2 = 2 * np.dot(np.dot(x, l_pre_x), pre_x_at_a)
-    print(sympy.expand(obj), obj_2)
-    return obj - obj_2
+    obj = obj - obj_2
+    obj = sympy.expand(obj)
+    return obj
