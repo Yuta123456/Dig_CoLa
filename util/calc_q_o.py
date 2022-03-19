9ced8a8f2196731b765cb9a10af5911facfc6431
Yuta123456 <yuuta09090530@icloud.com>
2022-03-19 17:34:27 +0900

    引数名を変更

--- util/calc_q_o.py
+++ util/calc_q_o.py
@@ -1,15 +1,16 @@
 import numpy as np
 import sympy
-def calc_q_o(node_cnt, f_x):
+def calc_q_o(node_cnt, exp):
     """
     最適化の際に必要なQ_oを導出する関数。
+    (1/2) * (X^T)Q_oXが最小化したい式になるようなQを計算。
 
     Parameters
     ----------
     node_cnt : int
         ノードの個数
     f_x : sympy.core.add.Add
-        最適化したい数式
+        最小化したい数式
 
     Returns
     -------
@@ -23,7 +24,7 @@ def calc_q_o(node_cnt, f_x):
             b = j+1
             x = sympy.Symbol(f'x_{a}')
             y = sympy.Symbol(f'x_{b}')
-            q_o[i][j] = f_x.coeff(x*y, 1)
+            q_o[i][j] = exp.coeff(x*y, 1)
             if i == j:
                 q_o[i][j] *= 2
     return q_o
