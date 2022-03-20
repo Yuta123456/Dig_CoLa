c596b06be10040743c11f61c39185ff0b227cbd9
Yuta123456 <yuuta09090530@icloud.com>
2022-03-20 16:04:10 +0900

    ストレス関数の計算を修正

--- util/calc_stress_x.py
+++ util/calc_stress_x.py
@@ -1,3 +1,9 @@
+def norm(point_1, point_2):
+    """
+    ベクトルに対してノルムを計算
+    """
+    return sum((point_1 - point_2) ** 2)**0.5
+
 def calc_stress_x(node_cnt, weight, dist, node_points):
     """
     ストレス関数を計算
@@ -6,6 +12,6 @@ def calc_stress_x(node_cnt, weight, dist, node_points):
     for i in range(node_cnt):
         for j in range(i+1, node_cnt):
                 # TODO: あってる気がしない。確認
-                stress_score += weight[i][j] * (sum((node_points[i] - node_points[j]) ** 2) - dist[i][j]) ** 2
+                stress_score += (norm(node_points[i], node_points[j]) - dist[i][j]) ** 2
 
     return stress_score
